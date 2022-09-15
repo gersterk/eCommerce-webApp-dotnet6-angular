8c85004d19a26e46103312d96d8a659742798154
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-15 13:03:33 +0200

    Service Registration Dependency Injections

--- AppAPI/Infrastructure/AppAPI.Persistence/ServiceRegistration.cs
+++ AppAPI/Infrastructure/AppAPI.Persistence/ServiceRegistration.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppAPI.Application.Repositories;
+using AppAPI.Persistence.Repositories;
 
 namespace AppAPI.Persistence
 {
@@ -16,7 +18,18 @@ namespace AppAPI.Persistence
         //via this injection the app will pass/transact the specific services once its interface was called
         public static void AddPersistenceService(this IServiceCollection services)
         {
-            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString), ServiceLifetime.Singleton);
+           
+            //Dependency Injections
+           
+            services.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
+            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
+
+            
 
         }
     }
---
+++ AppAPI/Presentation/AppAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using AppAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductReadRepository _productReadRepository;
+        private readonly IProductWriteRepository _productWriteRepository;
+
+        public ProductsController(IProductReadRepository productReadRepository, IProductWriteRepository productWriteRepository)
+        {
+            _productReadRepository = productReadRepository;
+            _productWriteRepository = productWriteRepository;
+        }
+
+        [HttpGet]
+        public async void Get()
+        {
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new(){Id=Guid.NewGuid(), Name="Product 1", Price=150, CreateDate= DateTime.UtcNow, Stock=35},
+                new(){Id=Guid.NewGuid(), Name="Product 2", Price=2, CreateDate= DateTime.UtcNow, Stock=1},
+                new(){Id=Guid.NewGuid(), Name="Product 3", Price=87, CreateDate= DateTime.UtcNow, Stock=17},
+                new(){Id=Guid.NewGuid(), Name="Product 4", Price=23, CreateDate= DateTime.UtcNow, Stock=19}
+
+            });
+            var counts = await _productWriteRepository.SaveAsync();
+        }
+    }
+}
