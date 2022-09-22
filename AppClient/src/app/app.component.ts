01f52c6147ec39ee02e5f983b620b921f1dcba82
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-22 19:09:21 +0200

    Toastr Costumization

--- AppClient/src/app/app.component.ts
+++ AppClient/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any
 @Component({
   selector: 'app-root',
@@ -7,5 +9,25 @@ declare var $: any
 })
 export class AppComponent {
   title = 'AppClient';
+
+  constructor(private toastrService : CustomToastrService){
+    toastrService.message("Hello", "Sores", {
+      messageType :ToastrMessageType.Info, 
+      position : ToastrPosition.BottomRight
+    });
+    toastrService.message("Hello", "Sores", {
+      messageType :ToastrMessageType.Success, 
+      position : ToastrPosition.BottomRight
+    });
+    toastrService.message("Hello", "Sores", {
+      messageType :ToastrMessageType.Warning, 
+      position : ToastrPosition.BottomRight
+    });
+    toastrService.message("Hello", "Sores", {
+      messageType :ToastrMessageType.Error, 
+      position : ToastrPosition.BottomRight
+    });
+
+  }
 }
 
