6116797effc1e12723619432f09aaab7ca19e5d1
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-23 22:52:39 +0200

    HttpClient Configuration Service tests

--- AppAPI/Presentation/AppAPI.API/Controllers/ProductsController.cs
+++ AppAPI/Presentation/AppAPI.API/Controllers/ProductsController.cs
@@ -1,11 +1,13 @@
 ﻿using AppAPI.Application.Repositories;
+using AppAPI.Application.ViewModels.Products;
 using AppAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace AppAPI.API.Controllers
 {
-    
+
     //Test controller so far
 
     [Route("api/[controller]")]
@@ -14,30 +16,66 @@ namespace AppAPI.API.Controllers
     {
         private readonly IProductReadRepository _productReadRepository;
         private readonly IProductWriteRepository _productWriteRepository;
-        private readonly ICustomerWriteRepository _customerWriterRepository;
-        private readonly IOrderWriteRepository _orderWriteRepository;
-        private readonly IOrderReadRepository _orderReadRepository;
+
 
         public ProductsController(
             IProductReadRepository productReadRepository,
-            IProductWriteRepository productWriteRepository,
-            IOrderWriteRepository orderWriteRepository,
-            ICustomerWriteRepository customerWriteRepository,
-            IOrderReadRepository orderReadRepository)
+            IProductWriteRepository productWriteRepository)
+
         {
 
             _productReadRepository = productReadRepository;
             _productWriteRepository = productWriteRepository;
-            _orderWriteRepository = orderWriteRepository;
-            _customerWriterRepository = customerWriteRepository;
-            _orderReadRepository = orderReadRepository;
+
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Ok("Selamsss");
+            return Ok(_productReadRepository.GetAll());
 
+        }
+        [HttpGet("{id}")]
+        //Paramethic GET 
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(await _productReadRepository.GetByIdAsync(id, false)); //tracking is false cuz we do not CRUD yet, for performance sake
+            
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(VM_Create_Product model) //post attributes shouldnt be applying any post proccess by The Entity : like (Product model) 
+        {
+            await _productWriteRepository.AddAsync(new()
+            {
+                Name = model.Name,
+                Price = model.Price,
+                Stock = model.Stock
+            });
+
+            await _productWriteRepository.SaveAsync();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(VM_Update_Product model)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(model.Id, false);
+            product.Name = model.Name;
+            product.Price = model.Price;
+            product.Stock = model.Stock;
+            await _productWriteRepository.SaveAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+            
+            return Ok();
         }
 
     }
