c79bcce6f3bc1235df15d864b46c21dbe257316c
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-25 19:20:30 +0200

    product add and list components

--- AppClient/src/app/admin/components/products/products.component.ts
+++ AppClient/src/app/admin/components/products/products.component.ts
@@ -21,35 +21,7 @@ export class ProductsComponent extends BaseComponent implements OnInit {
     this.httpClientService.get<Product []>({
       controller: "products"
     }).subscribe(data=> console.log(data));
-
-    // this.httpClientService.delete({
-    //   controller : "products"
-    // },"5af96ac6-3814-4af6-9e09-d4ac63f1d0f1").subscribe();
-    // this.httpClientService.post({
-
-    //   controller : "products"
-
-    // },{
-    //   name  : "pencil",
-    //   price: 20,
-    //   stock : 100
-
-    // }).subscribe();
-
-    // this.httpClientService.put({
-    //   controller : "products",
-    // },{
-    //   id: "6b741440-8028-4887-9697-d6b0573b05e4",
-    //   name: "colorful pencil",
-    //   stock: 1500,
-    //   price : 5.4
-
-    // }).subscribe()
-
-    //
-    // this.httpClientService.get({
-    //   fullEndPoint: "https://jsonplaceholder.typicode.com/posts",
-    // }).subscribe(data => console.log(data));
+    
    }
   }
 
