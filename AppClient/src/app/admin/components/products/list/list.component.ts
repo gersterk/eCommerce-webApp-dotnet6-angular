be1484fae413431334a64d4ae039c6f663576356
Baris Dogan <barisdogan.biz@gmail.com>
2022-09-30 00:00:24 +0200

    paginator

--- AppClient/src/app/admin/components/products/list/list.component.ts
+++ AppClient/src/app/admin/components/products/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material/table';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource, _MatTableDataSource } from '@angular/material/table';
 import { NgxSpinner, NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { List_Product } from 'src/app/contracts/list_products';
@@ -11,23 +12,30 @@ import { ProductService } from 'src/app/services/common/models/product.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponentv extends BaseComponent implements OnInit {
+export class ListComponent extends BaseComponent implements OnInit, AfterViewInit {
 
   constructor(spinner : NgxSpinnerService,private productService: ProductService, private alertifyService : AlertifyService) {
     super(spinner)
 
    }
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+
+  }
  displayedColumns: string[] = ['name', 'stock', 'price', 'createDate', 'updateDate'];
  dataSource : MatTableDataSource<List_Product>= null;
+@ViewChild(MatPaginator) paginator : MatPaginator;
 
-  ngOnInit(): void {
+  async ngOnInit() {
     this.showSpinner(SpinnerType.ballAtom);
     
-    this.productService.read(()=>this.hideSpinner(SpinnerType.ballAtom), errorMessage => this.alertifyService.message(errorMessage, {
+    const allProducts: List_Product[] = await this.productService.read(()=>this.hideSpinner(SpinnerType.ballAtom), errorMessage => this.alertifyService.message(errorMessage, {
       dismissOthers: true,
       messageType: MessageType.Error,
       position : Position.TopRight
     }))
+    this.dataSource = new MatTableDataSource<List_Product>(allProducts);
+
   }
 
 }
