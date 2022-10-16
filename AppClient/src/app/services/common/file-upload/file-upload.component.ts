097fbfdf887e7ffa3577f0e0a290c001d5cddf3b
Baris Dogan <barisdogan.biz@gmail.com>
2022-10-16 13:53:00 +0200

    upload arrangement

--- AppClient/src/app/services/common/file-upload/file-upload.component.ts
+++ AppClient/src/app/services/common/file-upload/file-upload.component.ts
@@ -10,51 +10,17 @@ export class FileUploadComponent {
 
     public files: NgxFileDropEntry[] = [];
 
-  public dropped(files: NgxFileDropEntry[]) {
+  public selectedFiles(files: NgxFileDropEntry[]) {
     this.files = files;
-    for (const droppedFile of files) {
 
-      // Is it a file?
-      if (droppedFile.fileEntry.isFile) {
-        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-        fileEntry.file((file: File) => {
+  const fileData : FormData = new FormData();
+    for(const file of files){
+    (file.fileEntry as FileSystemFileEntry).file((_file:File)=>{
+      fileData.append(_file.name, _file, file.relativePath);
 
-          // Here you can access the real file
-          console.log(droppedFile.relativePath, file);
+    });
 
-          /**
-          // You could upload it like this:
-          const formData = new FormData()
-          formData.append('logo', file, relativePath)
-
-          // Headers
-          const headers = new HttpHeaders({
-            'security-token': 'mytoken'
-          })
-
-          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
-          .subscribe(data => {
-            // Sanitized logo returned from backend
-          })
-          **/
-
-        });
-      } else {
-        // It was a directory (empty directories are added, otherwise only files)
-        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
-        console.log(droppedFile.relativePath, fileEntry);
-      }
     }
-  }
-
-  public fileOver(event){
-    console.log(event);
-  }
-
-  public fileLeave(event){
-    console.log(event);
+    
   }
 }
-
-
-
