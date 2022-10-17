c5ff72d4d0d7197a1860420ac681bf5f3ca4f80e
Baris Dogan <barisdogan.biz@gmail.com>
2022-10-17 18:52:27 +0200

    posting upload information to server

--- AppClient/src/app/services/common/file-upload/file-upload.component.ts
+++ AppClient/src/app/services/common/file-upload/file-upload.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgxFileDropEntry } from 'ngx-file-drop';
+import { HttpClientService } from '../http-client.service';
 
 @Component({
   selector: 'app-file-upload',
@@ -7,8 +9,12 @@ import { NgxFileDropEntry } from 'ngx-file-drop';
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent {
+  constructor(private httpclientService : HttpClientService) 
+  {  }
 
     public files: NgxFileDropEntry[] = [];
+
+    @Input() options : Partial<FileUploadOptions>;
 
   public selectedFiles(files: NgxFileDropEntry[]) {
     this.files = files;
@@ -20,7 +26,31 @@ export class FileUploadComponent {
 
     });
 
-    }
-    
   }
+    this.httpclientService.post({
+      controller: this.options.controller,
+      action : this.options.action,
+      queryString : this.options.queryString,
+      headers : new HttpHeaders({"responseType" : "blob"})
+
+    }, fileData).subscribe(data=>{
+
+    }, (errorResponse : HttpErrorResponse) =>{
+      
+    });
+
+
+  }
+
 }
+
+
+export class FileUploadOptions{
+  controller? : string;
+  action? : string;
+  queryString? : string;
+  explanation? : string;
+  accept? : string;
+
+
+ }
