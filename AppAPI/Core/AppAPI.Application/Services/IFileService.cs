c7ad9ff66cd485c79b6231bc5dece5772b7668e6
Baris Dogan <barisdogan.biz@gmail.com>
2022-11-07 20:11:16 +0100

    File Service/ File Renaming Operations

--- AppAPI/Core/AppAPI.Application/Services/IFileService.cs
+++ AppAPI/Core/AppAPI.Application/Services/IFileService.cs
@@ -13,7 +13,7 @@ namespace AppAPI.Application.Services
         //tuple return is used to store the path with the name
         //so one day the exact location of the product is needed,
         //hen we can be be achieving easily with the name and the path
-        Task<string> FileRenameAsync(string fileName);
+        
         Task<bool> CopyFileAsync(string path, IFormFile file);
 
 
--- AppAPI/Infrastructure/AppAPI.Infrastructure/Services/FileService.cs
+++ AppAPI/Infrastructure/AppAPI.Infrastructure/Services/FileService.cs
@@ -1,4 +1,5 @@
 ﻿using AppAPI.Application.Services;
+using AppAPI.Infrastructure.Operations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System;
@@ -36,10 +37,58 @@ namespace AppAPI.Infrastructure.Services
             
         }
 
-        public Task<string> FileRenameAsync(string fileName)
+
+
+        async Task<string> FileRenameAsync(string path, string fileName, bool first=true)
         {
-            throw new NotImplementedException();
+            string newFileName = await Task.Run<string>(async ()=>
+            {
+                string extension = Path.GetExtension(fileName);
+                string newFileName = string.Empty;
+
+
+            if (first)
+            {
+                string oldName = Path.GetFileNameWithoutExtension(fileName);
+                newFileName = $"{NameOperation.CharacterRegulatory(oldName)}{extension}";
+            }
+            else
+            {
+                newFileName = fileName;
+                int indexNo1 = newFileName.IndexOf("-");
+                    if (indexNo1 ==-1)
+                    {
+                        newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2{extension}";
+                        return newFileName;
+
+
+                    }
+
+                    else
+                    {
+                        int indexNo2 = newFileName.IndexOf(".");
+                        string fileNo = newFileName.Substring(indexNo1, indexNo2 - indexNo1 - 1);
+                        int _fileNo = int.Parse(fileNo);
+                        _fileNo++;
+
+                        newFileName = newFileName.Remove(indexNo1, indexNo2 - indexNo2 - 1)
+                        .Insert(indexNo1, _fileNo.ToString());
+
+                    }
+                }
+
+                if (File.Exists($"{path}\\{newFileName}"))
+                    return await FileRenameAsync(path, newFileName, false);
+                else
+                    return newFileName;
+
+               
+            });
+
+            return newFileName;
+
         }
+
 
         public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
         {
@@ -49,8 +98,9 @@ namespace AppAPI.Infrastructure.Services
                 Directory.CreateDirectory(uploadPath);
 
             List<(string fileName, string path)> datas = new();
-
             List<bool> results = new();
+
+
             foreach (IFormFile file in files)
             {
                 string  fileNewName = await FileRenameAsync(file.FileName);
@@ -59,6 +109,8 @@ namespace AppAPI.Infrastructure.Services
                 datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
                 results.Add(result);
             }
+            
+            
             if (results.TrueForAll(r=> r.Equals(true)))
             {
                 return datas;
