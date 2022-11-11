7c9735c8e490f7ee70e10cb91531cdce4a45b5c4
Baris Dogan <barisdogan.biz@gmail.com>
2022-11-11 19:39:12 +0100

    TPH for discriminating the files. Table Per Hierarchy

---
+++ AppAPI/Core/AppAPI.Domain/Entities/File.cs
@@ -0,0 +1,16 @@
+﻿using AppAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppAPI.Domain.Entities
+{
+    public class File : BaseEntity
+    {
+        [NotMapped]
+        public override DateTime UpdatedDate { get => base.UpdatedDate; set => base.UpdatedDate = value; }
+    }
+}
